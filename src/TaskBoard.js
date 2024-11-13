33ba46496968a900d3deb7047c82eebe85d780b2
unknown <sergi.zagriva@gmail.com>
2024-11-13 17:45:08 +0000

    Updated functionality, added styling.

--- src/TaskBoard.js
+++ src/TaskBoard.js
@@ -9,17 +9,22 @@ function TaskBoard() {
         { id: 3, title: "Task 3", status: "complete"},
     ]);
 
-    // New task state
-    const [newTask, setNewTask] = useState("");
+    // New upcoming task state
+    const [newUpcomingTask, setNewUpcomingTask] = useState("");
 
-    // Adding new task
-    const addTask = () => {
+    // New current task state
+    const [newCurrentTask, setNewCurrentTask] = useState("");
 
-        if (newTask.trim() === "") return; // If empty
+    // Adding new upcoming task
+    const addUpcomingTask = (e) => {
+
+        e.preventDefault() // Prevents the page from refreshing to allow ENTER to work
+
+        if (newUpcomingTask.trim() === "") return; // If empty
 
         const newTaskObject = {
             id: tasks.length + 1, // Task id - !UPDATE LATER!
-            title: newTask,       // Input value
+            title: newUpcomingTask,       // Input value
             status: "upcoming"    // Status by default
         };
 
@@ -27,7 +32,28 @@ function TaskBoard() {
         setTasks([...tasks, newTaskObject]);
 
         // Clear the input field
-        setNewTask("");
+        setNewUpcomingTask("");
+
+    };
+
+    // Adding new current task
+    const addCurrentTask = (e) => {
+
+        e.preventDefault() // Prevents the page from refreshing to allow ENTER to work
+
+        if (newCurrentTask.trim() === "") return; // If empty
+
+        const newTaskObject = {
+            id: tasks.length + 1, // Task id - !UPDATE LATER!
+            title: newCurrentTask,       // Input value
+            status: "current"    // Status by default
+        };
+
+        // Update task array with the new task
+        setTasks([...tasks, newTaskObject]);
+
+        // Clear the input field
+        setNewCurrentTask("");
 
     };
 
@@ -39,47 +65,66 @@ function TaskBoard() {
     
     return (
         <div className="taskboard">
-            <div className="taskColumn">
+            <div className="taskColumnUpcoming">
                 <h1>Upcoming Tasks</h1>
                 {tasks.filter(task => task.status === "upcoming").map(task => (
-                    <div key={task.id}>
+                    <div className="taskItem" key={task.id}>
                         {task.title}{" "}
                         <button onClick={() => deleteTask(task.id)}>X</button>
                     </div>
                 ))}
+
+                <form id="newUpcomingTaskForm">
+                    <input 
+                    id="newUpcomingTaskInput" 
+                    type="text" 
+                    value={newUpcomingTask} //Input value to state
+                    onChange={(e) => setNewUpcomingTask(e.target.value)} // Update state live
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            addUpcomingTask(e);
+                        }
+                    }}
+                    />
+                <button type="button" onClick={addUpcomingTask}>Add Task</button>    
+                </form>
             </div>
 
-            <div className="taskColumn">
+            <div className="taskColumnCurrent">
                 <h1>Current Tasks</h1>
                 {tasks.filter(task => task.status === "current").map(task => (
-                    <div key={task.id}>
+                    <div className="taskItem" key={task.id}>
                         {task.title}{" "}
                         <button onClick={() => deleteTask(task.id)}>X</button>
-                    </div>   
+                    </div>     
                 ))}
 
+                <form id="newCurrentTaskForm">
+                    <input 
+                    id="newCurrentTaskInput" 
+                    type="text" 
+                    value={newCurrentTask} //Input value to state
+                    onChange={(e) => setNewCurrentTask(e.target.value)} // Update state live
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            addCurrentTask(e);
+                        }
+                    }}
+                    />
+                <button type="button" onClick={addCurrentTask}>Add Task</button>    
+                </form>
+
             </div>
-            <div className="taskColumn">
+            <div className="taskColumnComplete">
                 <h1>Completed Tasks</h1>
                 {tasks.filter(task => task.status === "complete").map(task => (
-                    <div key={task.id}>
+                    <div className="taskItem" key={task.id}>
                         {task.title}{" "}
                         <button onClick={() => deleteTask(task.id)}>X</button>
                     </div>  
                 ))}
 
             </div>
-
-            <form id="newTaskForm">
-                <input 
-                    id="newTaskInput" 
-                    type="text" 
-                    value={newTask} //Input value to state
-                    onChange={(e) => setNewTask(e.target.value)} // Update state live
-                />
-            <button type="button" onClick={addTask}>Add Task</button>    
-            </form>
-
         </div>        
     );
 }
